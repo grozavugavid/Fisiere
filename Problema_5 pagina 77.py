bbb8ef6ad67855ab585e2a13367aafa17d871150
grozavudavid <70814477+grozavugavid@users.noreply.github.com>
2021-12-01 20:27:54 +0200

    Update Problema_5 pagina 77.py

--- Problema_5 pagina 77.py
+++ Problema_5 pagina 77.py
@@ -6,4 +6,10 @@ f=open('c/Users/STATIE-6-C100/Desktop/Caracter.txt','r')
 x=f.read()
 f.close()
 print(x)
-vocal
+vocal=[]
+for i in range(0,len(x)):
+  if x[i]=='o' or 'a' or 'u' or 'e' or 'i':
+    vocal[i]!=x[i]
+f=open('c/Users/STATIE-6-C100/Desktop/Caracter.txt','w')
+f.write(vocal)
+f.close()
